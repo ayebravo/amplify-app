467f41b9f2d9261a80b9812bce2743fcee983ecf
ayebravo <ayelenbravo@gmail.com>
2022-02-01 23:27:55 -0600

    Did an amplify push
    Deployed first feature

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyappspring2022e17ebbde": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
